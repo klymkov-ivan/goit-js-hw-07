db456e5ee70518095a849ad148d9c835954b1c7c
klymkov_ivan <klymkov90@gmail.com>
2025-06-27 15:34:56 +0300

    task-1 done

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+const mainCategories = document.querySelector("#categories");
+
+const categories = document.querySelectorAll(".item");
+console.log(`Number of categories: ${categories.length}`);
+
+categories.forEach((category) => {
+  const title = category.querySelector("h2").textContent;
+  const itemsCount = category.querySelectorAll("ul li").length;
+
+  console.log(`Category: ${title}`);
+  console.log(`Elements: ${itemsCount}`);
+});
