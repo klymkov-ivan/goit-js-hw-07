de643530c6f44e80fde532ca42b2270e0b214c24
klymkov_ivan <klymkov90@gmail.com>
2025-06-28 00:16:41 +0300

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,20 @@
+const loginForm = document.querySelector(".login-form");
 
+let userInfo = {};
+
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const email = loginForm.elements.email.value.trim();
+  const password = loginForm.elements.password.value.trim();
+
+  if (email === "" || password === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  userInfo = { email, password };
+  console.log(userInfo);
+
+  loginForm.reset();
+});
