cc215f39dfd86ea5b1f447d0c7b118de4fe82025
klymkov_ivan <klymkov90@gmail.com>
2025-06-28 10:48:19 +0300

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,15 @@
+const widget = document.querySelector(".widget");
+const changeColor = document.querySelector(".change-color");
+const colorSpan = document.querySelector(".color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+changeColor.addEventListener("click", () => {
+  const newColor = getRandomHexColor();
+  widget.style.backgroundColor = newColor;
+  colorSpan.textContent = newColor;
+});
